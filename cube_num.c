5f81031e8ffc35ae722bac63eac66b9c9a63bc29
Vishal Pansare <51355863+vishalpansare5600@users.noreply.github.com>
2023-03-17 12:25:39 +0530

    Add files via upload

---
+++ cube_num.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+	void main()
+{	
+	int number;
+	printf("enter the number:");
+	scanf("%d",&number);
+	printf("cube of the given no is %d",number*number*number);
+
+getch();
+return 0;
+}
+
